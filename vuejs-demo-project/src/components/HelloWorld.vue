1e5ed58f455841044b046b51772bbe1bb7f7815c
mymywas <mynvt95@gmail.com>
2018-01-30 22:15:02 +0700

    add method

--- vuejs-demo-project/src/components/HelloWorld.vue
+++ vuejs-demo-project/src/components/HelloWorld.vue
@@ -1,113 +1,45 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+    <h1>{{ msg1 }}</h1>
+    <h1>{{ msg2 }}</h1>
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
       </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    </ol>
+    <span v-if="seen">Now you see me</span>
+    <p>{{ message }}</p>
+    <button @click="checkFunc">Reverse Message</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        msg1: 'Welcome to Your Vue.js App',
+        msg2: 'Welcome to Your Vue.js App',
+        todos: [
+          {text: 'Learn JavaScript'},
+          {text: 'Learn Vue'},
+          {text: 'Build something awesome'}
+        ],
+        seen: true,
+        message: 'Hello Vue.js!'
+      }
+    },
+    methods: {
+      checkFunc(){
+        console.log('All is working!');
+      },
+    },
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
